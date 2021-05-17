e909e1faa3ff5125cda2f8512f7aa1e006657161
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-05-17 18:13:42 -0300

    accordion transformed into class

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 /* eslint-disable import/extensions */
 import ScrollSuave from './modules/scroll-suave.js';
-import initAnimacaoScroll from './modules/scroll-animacao.js';
-import initAccordion from './modules/accordion.js';
+import Accordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
@@ -10,9 +9,13 @@ import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
 import initFetchAnimais from './modules/fetch-animais.js';
 import initFetchBitcoin from './modules/fetch-bitcoin.js';
+import initAnimacaoScroll from './modules/scroll-animacao.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
+
+const accordion = new Accordion('[data-anime="accordion"] dt');
+accordion.init();
 
 initAnimacaoScroll();
 initAccordion();
