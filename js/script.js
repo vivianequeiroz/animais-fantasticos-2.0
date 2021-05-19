498b13e855df43a20502272fb6527e7093981c33
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-05-19 18:08:19 -0300

    transforming tooltip into class

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ import ScrollSuave from './modules/scroll-suave.js';
 import Accordion from './modules/accordion.js';
 import TabNav from './modules/tabnav.js';
 import initModal from './modules/modal.js';
-import initTooltip from './modules/tooltip.js';
+import ToolTip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
@@ -20,10 +20,12 @@ accordion.init();
 const tabNav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section');
 tabNav.init();
 
+const toolTip = new ToolTip('[data-tooltip]');
+toolTip.init();
+
 initAnimacaoScroll();
 
 initModal();
-initTooltip();
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
