1364da9af84acb7e787c14a114550cb141007e3c
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-09-02 18:02:22 -0300

    chore: preventing double events at mobile menu

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -15,7 +15,8 @@ export default class MenuMobile {
     this.openMenu = this.openMenu.bind(this);
   }
 
-  openMenu() {
+  openMenu(event) {
+    event.preventDefault(); // prevents touchstart and click events to be both added
     this.menuList.classList.add(this.activeClass);
     this.menuButton.classList.add(this.activeClass);
     outsideClick(this.menuList, this.events, () => {
